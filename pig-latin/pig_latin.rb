ca0e4d56443de915970579d0b187839b9aca2dc0
Luca Nioi <lnioi@thoughtworks.com>
2019-06-06 11:55:14 +0200

    Updated pig-latin

--- pig-latin/pig_latin.rb
+++ pig-latin/pig_latin.rb
@@ -1,23 +1,19 @@
 module PigLatin
-  extend self
-
   VOWELS_REGEXP = /[aeio]|y(?:[^aeiou]|\b)|(?<!q)u|\Ax[^aeiou]/
   PIGTAIL = 'ay'.freeze
 
-  def translate(phrase)
-    phrase.split.map do |word|
-      to_pig_latin(word.downcase)
-    end.join("\s")
+  class << self
+    def translate(phrase)
+      phrase.split.map do |word|
+        to_pig_latin(word.downcase)
+      end.join("\s")
+    end
+
+    private
+
+    def to_pig_latin(word)
+      vowel_index = word =~ VOWELS_REGEXP
+      word[vowel_index...word.size] + word[0...vowel_index] + PIGTAIL
+    end
   end
-
-  private
-
-  def to_pig_latin(word)
-    vowel_index = word =~ VOWELS_REGEXP
-    word[vowel_index...word.size] + word[0...vowel_index] + PIGTAIL
-  end
-end
-
-module BookKeeping
-  VERSION = 2
 end
