4cd64cb91afc894056951b43550509e88a22ea75
Luca Nioi <lnioi@thoughtworks.com>
2019-06-12 01:50:29 +0200

    Refactored

--- scale-generator/scale_generator.rb
+++ scale-generator/scale_generator.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 class Scale
-  USE_SHARPS = %w(C G D A E B F# a e b f# c# g# d#).freeze
-  SCALE_WITH_SHARPS = %w(A A# B C C# D D# E F F# G G#).freeze
-  SCALE_WITH_FLATS = %w(A Bb B C Db D Eb E F Gb G Ab).freeze
+  USE_SHARPS = %w(C G D A E B F# a e b f# c# g# d#)
+  SHARP_SCALE = %w(A A# B C C# D D# E F F# G G#)
+  FLAT_SCALE = %w(A Bb B C Db D Eb E F Gb G Ab)
 
-  CHROMATIC_INTERVALS = 'mmmmmmmmmmmm'.freeze
+  CHROMATIC_STEPS = 'mmmmmmmmmmmm'
 
-  INTERVALS = {
+  STEPS = {
     'm' => 1,
     'M' => 2,
     'A' => 3
-  }.freeze
+  }
 
-  def initialize(tonic, scale_type, intervals = nil)
+  def initialize(tonic, scale_type, steps = nil)
     @tonic      = tonic
     @scale_type = scale_type
-    @intervals  = intervals || CHROMATIC_INTERVALS
+    @steps      = steps || CHROMATIC_STEPS
   end
 
   def name
@@ -22,21 +24,22 @@ class Scale
   end
 
   def pitches
-    scale.rotate(offset).values_at(*normalize_intervals)
+    scale.rotate(offset).values_at(*intervals_from_tonic)
   end
 
   private
 
-  attr_reader :tonic, :scale_type, :intervals
+  attr_reader :tonic, :scale_type, :steps
 
   def scale
-    USE_SHARPS.include?(tonic) ? SCALE_WITH_SHARPS : SCALE_WITH_FLATS
+    USE_SHARPS.include?(tonic) ? SHARP_SCALE : FLAT_SCALE
   end
 
-  def normalize_intervals
-    intervals.chars.take(intervals.size - 1).reduce([0]) do |normalized_intervals, interval|
-      normalized_intervals << normalized_intervals.last + INTERVALS[interval]
-    end
+  def intervals_from_tonic
+    steps.chars.take(steps.size - 1)
+      .reduce([0]) do |intervals, step|
+        intervals + [intervals.last + STEPS[step]]
+      end
   end
 
   def offset
